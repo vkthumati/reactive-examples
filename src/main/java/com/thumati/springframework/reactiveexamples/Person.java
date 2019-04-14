9c827f4815094711c705f3fcfa1f5a9600bc0446
vkthumati <info2vasanth@gmail.com>
2019-04-13 23:03:58 -0500

    Initial Commit

---
+++ src/main/java/com/thumati/springframework/reactiveexamples/Person.java
@@ -0,0 +1,19 @@
+package com.thumati.springframework.reactiveexamples;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String sayMyName() {
+        return "My Name is " + firstName + " " + lastName + ".";
+    }
+
+}
---
+++ src/test/java/com/thumati/springframework/reactiveexamples/ReactiveExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thumati.springframework.reactiveexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveExamplesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
